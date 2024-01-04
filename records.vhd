405be1559be49f5eaf4dbb74c1a9195b9ca9b2d0
Trash-Child <dicte.mj.98@gmail.com>
2024-01-04 14:22:37 +0100

    unclock decoder

--- records.vhd
+++ records.vhd
@@ -10,7 +10,8 @@ package records_pkg is
 		ALUsrc1: std_logic;						
 		ALUsrc2: std_logic;
 		ALUop: std_logic_vector(3 downto 0);
-
+		REG_we: std_logic;						-- Register file write enable
+		immediate: std_logic_vector (31 downto 0); 	-- immediate value
 	end record t_from_decoder;
 
 
