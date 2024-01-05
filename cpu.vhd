1745f8e6230f7fa444d48fb49e8a7a9bdad36bea
Evangelos Lamprou <e.lamprou@upnet.gr>
2024-01-05 15:04:13 +0100

    connect fetch and decode stage in cpu

--- cpu.vhd
+++ cpu.vhd
@@ -1,12 +1,29 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.records_pkg.all;
+
 
 entity cpu is 
     port (clock : in std_logic);
 end cpu;
 
 architecture behavioral of cpu is 
+
+component reg_file is 
+	generic(
+	B: integer:= 32;
+	W: integer:= 5
+	);
+	port (
+		clock, we: in std_logic;
+		instruction: in std_logic_vector(31 downto 0);
+		w_addr: in std_logic_vector(4 downto 0);
+		w_data: in std_logic_vector(B-1 downto 0);
+		rs1_out, rs2_out: out std_logic_vector(B-1 downto 0)
+	);
+end component;
+
 
 component memory is
   port
@@ -21,7 +38,7 @@ end component;
 
 component fetcher is
     port (
-     clk, branch, reset, we : in std_logic;
+     clock, branch, reset, we : in std_logic;
      data_input: in std_logic_vector (31 downto 0);
      branch_address: in std_logic_vector (31 downto 0);
      pc_out : out std_logic_vector (31 downto 0);
@@ -31,23 +48,37 @@ end component;
 
 component reg_ifid is
 	port (
-	clk, en, clear : in std_logic;						-- enable, clear
+	clock, en, clear : in std_logic;						-- enable, clear
 	in_pc, in_pc4, in_instruction : in std_logic_vector (31 downto 0);	-- register inputs
 	out_pc, out_pc4, out_instruction : out std_logic_vector (31 downto 0)	-- register outputs
 	); 
 end component;
 
+component decoder is
+	port (
+	instruction: in std_logic_vector(31 downto 0);
+	decoder_out: out t_from_decoder;
+  REG_rs1, REG_rs2: out std_logic_vector (4 downto 0)
+	);
+end component;
+
+
 -- Signals for interconnection
     signal branch, reset, we, en, clear : std_logic;
     signal data_input, branch_address, in_pc4 : std_logic_vector(31 downto 0);
     signal pc_out, instruction, out_pc, out_pc4, out_instruction : std_logic_vector(31 downto 0);
+		signal decoder_out : t_from_decoder;
+		signal REG_rs1, REG_rs2 : std_logic_vector (4 downto 0);
+		signal w_addr : std_logic_vector (4 downto 0);
+		signal w_data : std_logic_vector (31 downto 0);
+		signal reg1_out, reg2_out : std_logic_vector (31 downto 0);
 
 begin
 
-    -- Instruction Fetch
+    -- Fetcher
     fetcher_inst: fetcher
         port map (
-            clk => clock,
+            clock => clock,
             branch => branch,
             reset => reset,
             we => we,
@@ -57,11 +88,27 @@ begin
             instruction => instruction
         );
 
-    -- Register IF/ID
+  	-- Register File
 
+		reg_file_inst : reg_file 
+				generic map (
+						B => 32,
+						W => 5
+				)
+				port map (
+						clock => clock,
+						we => we,
+						instruction => out_instruction,
+						w_addr => w_addr,
+						w_data => w_data,
+						rs1_out => reg1_out,
+						rs2_out => reg2_out
+				);
+
+    -- Register IF/ID
      reg_ifid_inst: reg_ifid
         port map (
-            clk => clock,
+            clock => clock,
             en => en,
             clear => clear,
             in_pc => pc_out,
@@ -72,11 +119,43 @@ begin
             out_instruction => out_instruction
         );
 
-    
-    -- Instruction Decode
+		-- Decoder
+		decoder_inst: decoder 
+				port map (
+						instruction => out_instruction,
+						decoder_out => decoder_out,
+						REG_rs1 => REG_rs1,
+						REG_rs2 => REG_rs2
+			 );
 
-    -- Execute 
+			reset <= '0';
+			branch <= '0';
+			en <= '1';
+			clear <= '0';
+			branch_address <= x"00000000";
+			we <= '0';
 
-    -- Memory and Write Back
+
+		 process(clock) is
+		 begin 
+		
+		   if (rising_edge(clock)) then
+
+					 report "PC: " & to_string(out_pc);
+					 report "Instruction: " & to_string(out_instruction);
+					 report "Immediate: " & to_string(decoder_out.immediate);
+					 report "Register 1: " & to_string(reg1_out);
+					 report "Register 2: " & to_string(reg2_out);
+			 
+			-- Instruction Fetch
+
+			-- Instruction Decode
+
+			-- Execute 
+
+			-- Memory and Write Back
+		  
+		  end if;
+		 end process;
 
 end behavioral;
--- makefile
+++ makefile
@@ -9,3 +9,9 @@ execute:
 decode:
 	nvc --std=2008 -a records.vhd
 	nvc --std=2008 -a decode.vhd test/decode_tb.vhd -e decode_tb -r 
+
+cpu:
+	nvc --std=2008 -a records.vhd
+	nvc --std=2008 -a decode.vhd 
+	nvc --std=2008 -a fetch.vhd 
+	nvc --std=2008 -a cpu.vhd test/cpu_tb.vhd -e cpu_tb -r
