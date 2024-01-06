272d25ef1a4f4dc5d2d46aefdaaab8c869483570
Evangelos Lamprou <e.lamprou@upnet.gr>
2024-01-06 23:35:54 +0100

    add id/ex register and from_id record

--- records.vhd
+++ records.vhd
@@ -6,14 +6,19 @@ package records_pkg is
 	
 	-- Outputs from decoder
 	type t_from_decoder is record
-		rd: std_logic_vector(4 downto 0);		-- destination register
 		ALUsrc1: std_logic;						
 		ALUsrc2: std_logic;
 		ALUop: std_logic_vector(3 downto 0);
+		rd: std_logic_vector(4 downto 0);		-- destination register
 		REG_we: std_logic;						-- Register file write enable
 		immediate: std_logic_vector (31 downto 0); 	-- immediate value
-		REG_rs1: std_logic_vector (4 downto 0);
-		REG_rs2: std_logic_vector (4 downto 0);
 	end record t_from_decoder;
+
+	type t_from_id is record
+			decoded: t_from_decoder;
+			reg_rs1: std_logic_vector (31 downto 0);
+			reg_rs2: std_logic_vector (31 downto 0);
+			-- pc: std_logic_vector (31 downto 0); -- not needed for now (only implemented for branch)
+  end record t_from_id;
 
   end package records_pkg;
