55178d4fdab29414398df130bbf89c86bf71960f
Trash-Child <dicte.mj.98@gmail.com>
2024-01-08 15:15:01 +0100

    signal renaming

--- records.vhd
+++ records.vhd
@@ -7,13 +7,12 @@ package records_pkg is
 	-- Outputs from decoder
 	type t_from_decoder is record
 		rd: std_logic_vector(4 downto 0);		-- destination register
-		ALUsrc1: std_logic;						
-		ALUsrc2: std_logic;
-		ALUop: std_logic_vector(3 downto 0);
+		ALU_src1_ctrl: std_logic;						
+		ALU_src2_ctrl: std_logic;
+		ALU_op_ctrl: std_logic_vector(3 downto 0);
 		REG_we: std_logic;						-- Register file write enable
 		immediate: std_logic_vector (31 downto 0); 	-- immediate value
-		REG_rs1: std_logic_vector (4 downto 0);
-		REG_rs2: std_logic_vector (4 downto 0);
+
 	end record t_from_decoder;
 
   end package records_pkg;
