3d7168e2ae50d8c20912504f45a758f3039010e6
amal-araweelo <amal-araweelo@github.com>
2024-01-09 14:49:49 +0100

    fetch done

--- fetch.vhd
+++ fetch.vhd
@@ -6,57 +6,55 @@ use ieee.numeric_std.all;
 
 -- read binary file
 -- Hardcode an addi instruction
--- addi x1, x0, 10 -> 0x00A00093
 
-entity fetcher is 
-    port (
-     clock, branch, reset, we : in std_logic;
-     pc_out : out std_logic_vector (31 downto 0);
-     data_input: in std_logic_vector (31 downto 0);
-     branch_address: in std_logic_vector (31 downto 0);
-     instruction: out std_logic_vector (31 downto 0)
-     );
-end fetcher;
-
-architecture behavioral of fetcher is 
-signal my_instruction : std_logic_vector (31 downto 0) := x"00310093"; -- addi x1, x2, 3
-signal length : std_logic_vector(31 downto 0) := x"00000004";
-signal reg : std_logic_vector(31 downto 0) := x"00000000";
-signal pc_next : std_logic_vector(31 downto 0);
-
-component memory is
+entity fetcher is
   port
   (
-    clk    : in std_logic;
-    we     : in std_logic;
-    rdData : out std_logic_vector(31 downto 0);
-    wrData : in std_logic_vector(31 downto 0);
-    addr   : in std_logic_vector(31 downto 0)
+    clk, sel_pc, clr, en : in std_logic;
+    pc                   : out std_logic_vector (31 downto 0);
+    -- data_input: in std_logic_vector (31 downto 0);
+    branch_addr : in std_logic_vector (31 downto 0);
+    instr       : out std_logic_vector (31 downto 0)
   );
-end component;
+end fetcher;
+
+architecture behavioral of fetcher is
+  signal length  : std_logic_vector(31 downto 0) := x"00000004";
+  signal pc_reg  : std_logic_vector(31 downto 0) := x"00000000";
+  signal pc_next : std_logic_vector(31 downto 0);
+
+  component instr_mem is
+    port
+    (
+      -- Inputs
+      clk      : in std_logic                     := '0'; -- clock
+      MEM_addr : in std_logic_vector(31 downto 0) := (others => '0'); -- address (it is the value stored in register 2)
+
+      -- Outputs
+      MEM_instr_out : out std_logic_vector(31 downto 0) := (others => '0')
+    );
+  end component;
 
 begin
+  inst_instr_mem : instr_mem port map
+    (clk, pc, instr);
 
--- Instruction_Memory: memory port map (clock, we, instruction, data_input, pc); -- TODO: Add this
+  process (sel_pc, pc_reg, branch_addr) begin
+    if (sel_pc = '1') then
+      pc_next <= branch_addr;
+    else
+      pc_next <= std_logic_vector(unsigned(pc_reg) + unsigned(length));
+    end if;
+  end process;
 
-process(branch, reg, branch_address) begin
-		if (branch = '1') then
-				pc_next <= branch_address;
-		else
-				pc_next <= std_logic_vector(unsigned(reg) + unsigned(length));
-		end if;
-end process;
+  process (clk, clr, en)
+  begin
+    if (clr = '1') then
+      pc_reg <= (others => '0');
+    elsif (rising_edge(clk) and en = '1') then
+      pc_reg <= pc_next;
+    end if;
+  end process;
 
-process (clock, reset)
-begin 
-  if (reset = '1') then
-					reg <= (others => '0');
-  elsif (rising_edge(clock)) then
-					reg <= pc_next;
-  end if;
-end process;
-
-pc_out <= reg;
-instruction <= my_instruction;
-
+  pc <= pc_reg;
 end behavioral;
--- test/fetch_tb.vhd
+++ test/fetch_tb.vhd
@@ -4,140 +4,138 @@ use ieee.std_logic_arith.all;
 use std.env.stop;
 
 entity fetcher_tb is
-    -- Testbench has no ports
+  -- Testbench has no ports
 end fetcher_tb;
 
-architecture behavior of fetcher_tb is 
+architecture behavior of fetcher_tb is
 
-    -- Component Declaration for the Unit Under Test (UUT)
-    component fetcher
-        port (
-            clock, branch, reset, we : in std_logic;
-            pc_out : out std_logic_vector (31 downto 0);
-            data_input: in std_logic_vector (31 downto 0);
-            branch_address: in std_logic_vector (31 downto 0);
-            instruction: out std_logic_vector (31 downto 0)
-        );
-    end component;
+  -- Component Declaration for the Unit Under Test (UUT)
+  component fetcher
+    port
+    (
+      clk, sel_pc, clr, en : in std_logic;
+      pc                   : out std_logic_vector (31 downto 0);
+      branch_addr          : in std_logic_vector (31 downto 0);
+      instr                : out std_logic_vector (31 downto 0)
+    );
+  end component;
 
-    -- Inputs
-    signal clock : std_logic := '0';
-    signal branch : std_logic := '0';
-    signal reset : std_logic := '1';
-    signal we : std_logic := '0';
-    signal data_input : std_logic_vector(31 downto 0) := (others => '0');
-    signal branch_address : std_logic_vector(31 downto 0) := (others => '0');
+  -- Inputs
+  signal clk         : std_logic                     := '0';
+  signal sel_pc      : std_logic                     := '0';
+  signal clr         : std_logic                     := '1';
+  signal en          : std_logic                     := '0';
+  signal branch_addr : std_logic_vector(31 downto 0) := (others => '0');
 
-    -- Outputs
-    signal pc_out : std_logic_vector(31 downto 0);
-    signal instruction : std_logic_vector(31 downto 0);
+  -- Outputs
+  signal pc_out : std_logic_vector(31 downto 0);
+  signal instr  : std_logic_vector(31 downto 0);
 
-    -- Clock period definitions
-    constant clock_period : time := 10 ns;
+  -- Clock period definitions
+  constant clk_period : time := 10 ns;
 
 begin
 
-    -- Instantiate the Unit Under Test (UUT)
-    uut: fetcher port map (
-        clock, branch, reset,
-        we,
-        pc_out,
-        data_input,
-        branch_address,
-        instruction
-    );
+  -- Instantiate the Unit Under Test (UUT)
+  uut : fetcher port map
+  (
+    clk, sel_pc, clr,
+    en,
+    pc_out,
+    branch_addr,
+    instr
+  );
 
-    -- Clock process definitions
-    clock_process : process
-    begin
-        clock <= '0';
-        wait for clock_period/2;
-        clock <= '1';
-        wait for clock_period/2;
-    end process;
+  -- Clock process definitions
+  clk_process : process
+  begin
+    clk <= '0';
+    wait for clk_period/2;
+    clk <= '1';
+    wait for clk_period/2;
+  end process;
 
-    -- Stimulus process
-    stim_proc: process
-    begin
-        reset <= '1';
-        branch <= '0';
-	we <= '1';
-        wait for clock_period;
-	
-	reset <= '0';
+  -- Stimulus process
+  stim_proc : process
+  begin
+    clr    <= '1';
+    sel_pc <= '0';
+    en     <= '1';
+    wait for clk_period;
 
-	-- Test 1
+    clr <= '0';
 
-	if pc_out = x"00000000" then
-		report "Test 1 [PASSED]";
-	else
-		report "Test 1 [FAILED]";
-		std.env.stop(1);
-	end if;
+    -- Test 1
 
-	-- Test 2
-	
-	reset <= '0';
-        wait for clock_period;
+    if pc_out = x"00000000" then
+      report "Test 1 [PASSED]";
+    else
+      report "Test 1 [FAILED]";
+      std.env.stop(1);
+    end if;
 
-	if pc_out = x"00000004" then
-	    report "Test 2 [PASSED]";
-	else
-	    report "Test 2 [FAILED]";
-	    std.env.stop(1);
-	end if;
+    -- Test 2
 
-	-- Test 3
-	
-        wait for clock_period;
+    clr <= '0';
+    wait for clk_period;
 
-	if pc_out = x"00000008" then
-	    report "Test 3 [PASSED]";
-	else
-	    report "Test 3 [FAILED]";
-	    std.env.stop(1);
-	end if;
+    if pc_out = x"00000004" then
+      report "Test 2 [PASSED]";
+    else
+      report "Test 2 [FAILED]";
+      std.env.stop(1);
+    end if;
 
-	-- Test 4
+    -- Test 3
 
-	wait for clock_period;
+    wait for clk_period;
 
-	if pc_out = x"0000000C" then
-	    report "Test 4 [PASSED]";
-	else
-	    report "Test 4 [FAILED]";
-	    std.env.stop(1);
-	end if;
+    if pc_out = x"00000008" then
+      report "Test 3 [PASSED]";
+    else
+      report "Test 3 [FAILED]";
+      std.env.stop(1);
+    end if;
 
-	-- Test 5
-        branch <= '1';
-	branch_address <= x"00000123";
-	wait for clock_period;
+    -- Test 4
 
-	if pc_out = x"00000123" then
-	    report "Test 5 [PASSED]";
-	else
-	    report "Test 5 [FAILED]";
-	    std.env.stop(1);
-	end if;
+    wait for clk_period;
 
-	-- Test 6
+    if pc_out = x"0000000C" then
+      report "Test 4 [PASSED]";
+    else
+      report "Test 4 [FAILED]";
+      std.env.stop(1);
+    end if;
 
-	wait for clock_period;
+    -- Test 5
+    sel_pc      <= '1';
+    branch_addr <= x"00000123";
+    wait for clk_period;
 
-        branch <= '0';
-	branch_address <= x"00000123";
-	wait for clock_period;
+    if pc_out = x"00000123" then
+      report "Test 5 [PASSED]";
+    else
+      report "Test 5 [FAILED]";
+      std.env.stop(1);
+    end if;
 
-	if pc_out = x"00000127" then
-	    report "Test 6 [PASSED]";
-	else
-	    report "Test 6 [FAILED]";
-	    std.env.stop(1);
-	end if;
+    -- Test 6
 
-        -- finish simulation
-	std.env.stop(0);
-    end process;
+    wait for clk_period;
 
+    sel_pc      <= '0';
+    branch_addr <= x"00000123";
+    wait for clk_period;
+
+    if pc_out = x"00000127" then
+      report "Test 6 [PASSED]";
+    else
+      report "Test 6 [FAILED]";
+      std.env.stop(1);
+    end if;
+
+    -- finish simulation
+    std.env.stop(0);
+  end process;
 end behavior;
