f6f3b8a83b900d3025af8e6fcbe75a4248fb6d31
Evangelos Lamprou <e.lamprou@upnet.gr>
2024-01-09 18:30:27 +0100

    update instruction memory

--- components/instr_mem.vhd
+++ components/instr_mem.vhd
@@ -9,7 +9,7 @@ entity instr_mem is
   port
   (
     -- Inputs
-    clk      : in std_logic                     := '0'; -- clock
+    clk      : in std_logic;
     MEM_addr : in std_logic_vector(31 downto 0) := (others => '0'); -- address (it is the value stored in register 2)
 
     -- Outputs
@@ -20,12 +20,18 @@ end instr_mem;
 architecture impl of instr_mem is
   type rom_type is array(2 ** 10 downto 0) -- 1 KiB
   of std_logic_vector (31 downto 0);
-  signal rom : rom_type;
+  signal rom : rom_type := (
+		    0 => x"00200113", -- addi x2, x0, 2
+		    1 => x"00300113", -- addi x2, x0, 2
+		    2 => x"FFFFFFFF", -- addi x1, x0, 1
+		    3 => x"00000000", -- addi x1, x0, 1
+		    others => (x"00000000") 
+    );
 
 begin
   process (clk) begin
     if (rising_edge(clk)) then
-      MEM_instr_out <= rom(to_integer(unsigned(MEM_addr))); -- data is always loaded from memory given addr
+      MEM_instr_out <= rom(to_integer(unsigned(MEM_addr(31 downto 2)))); -- data is always loaded from memory given addr
     end if;
   end process;
 end architecture;
