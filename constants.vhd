cad4004ef2330d5562b29ff3fc5be80b8fd65a11
amal-araweelo <amal-araweelo@github.com>
2024-01-09 11:35:44 +0100

    changes to constants

--- constants.vhd
+++ constants.vhd
@@ -29,18 +29,18 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
-package const_decoder is 
+package const_decoder is
   -- OPCODES
-  constant DEC_I_LOAD                 : std_logic_vector(6 downto 0) := "0000011";
-  constant DEC_I_ADD_SHIFT_LOGICOPS   : std_logic_vector(6 downto 0) := "0010011";
-  constant DEC_I_ADDW_SHIFTW          : std_logic_vector(6 downto 0) := "0011011";
-  constant DEC_I_JALR                 : std_logic_vector(6 downto 0) := "1100111";
-  constant DEC_R_OPS                  : std_logic_vector(6 downto 0) := "0110011";
-  constant DEC_R_OPSW                 : std_logic_vector(6 downto 0) := "0111011";
-  constant DEC_U_AUIPC                : std_logic_vector(6 downto 0) := "0010111";
-  constant DEC_U_LUI                  : std_logic_vector(6 downto 0) := "0110111";
-  constant DEC_S                      : std_logic_vector(6 downto 0) := "0100011";
-  constant DEC_UJ                     : std_logic_vector(6 downto 0) := "1101111";
+  constant DEC_I_LOAD               : std_logic_vector(6 downto 0) := "0000011";
+  constant DEC_I_ADD_SHIFT_LOGICOPS : std_logic_vector(6 downto 0) := "0010011";
+  constant DEC_I_ADDW_SHIFTW        : std_logic_vector(6 downto 0) := "0011011";
+  constant DEC_I_JALR               : std_logic_vector(6 downto 0) := "1100111";
+  constant DEC_R_OPS                : std_logic_vector(6 downto 0) := "0110011";
+  constant DEC_R_OPSW               : std_logic_vector(6 downto 0) := "0111011";
+  constant DEC_U_AUIPC              : std_logic_vector(6 downto 0) := "0010111";
+  constant DEC_U_LUI                : std_logic_vector(6 downto 0) := "0110111";
+  constant DEC_S                    : std_logic_vector(6 downto 0) := "0100011";
+  constant DEC_UJ                   : std_logic_vector(6 downto 0) := "1101111";
 
 end package const_decoder;
 
@@ -69,47 +69,51 @@ use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
 package records_pkg is
-	
-	-- Outputs from decoder
-	type t_decoder is record
-		rd: std_logic_vector(4 downto 0);		-- destination register
-		ALU_src1_ctrl: std_logic;						
-		ALU_src2_ctrl: std_logic;
-		ALU_op_ctrl: std_logic_vector(3 downto 0);
-		REG_we: std_logic;						-- Register file write enable
-		immediate: std_logic_vector (31 downto 0); 	-- immediate value
 
-	end record t_decoder;
+  -- Outputs from decoder
+  type t_decoder is record
+    REG_dst_idx   : std_logic_vector(4 downto 0); -- destination register
+    ALU_src1_ctrl : std_logic;
+    ALU_src2_ctrl : std_logic;
+    op_ctrl       : std_logic_vector(3 downto 0); -- operation control
+    REG_we        : std_logic; -- Register file write enable
+    imm           : std_logic_vector (31 downto 0); -- immediate value
 
-	type t_from_id is record
-			decoded: t_from_decoder;
-			reg_rs1: std_logic_vector (31 downto 0);
-			reg_rs2: std_logic_vector (31 downto 0);
-			-- pc: std_logic_vector (31 downto 0); -- not needed for now (only implemented for branch)
-  end record t_from_id;
+    -- Not implemented yet
+    WB_src_ctrt : std_logic_vector(1 downto 0);
+    MEM_op      : std_logic_vector(3 downto 0);
+    MEM_we      : std_logic;
+    do_jmp      : std_logic;
+    do_branch   : std_logic; -- not implemented, check if op_ctrl > 9
+    opcode      : std_logic_vector(6 downto 0); -- could be different
+
+    -- Havent decided on signal name
+    MEM_read : std_logic;
+
+  end record t_decoder;
 
   type t_ifid_reg is record
-    pc, instruction   : std_logic_vector (31 downto 0);	-- register inputs
+    pc, instruction : std_logic_vector (31 downto 0); -- register inputs
   end record t_ifid_reg;
 
   type t_idex_reg is record
-    decoder_record : t_decoder;
-    pc, reg_src_1, reg_src_2: std_logic_vector(31 downto 0);
+    decoder_record           : t_decoder;
+    pc, REG_src_1, REG_src_2 : std_logic_vector(31 downto 0);
   end record t_idex_reg;
 
   type t_exmem_reg is record
-    REG_we, MEM_we      : std_logic;
-    WB_src_ctrl:        : std_logic_vector(1 downto 0);
-    MEM_op              : std_logic_vector(3 downto 0);
-    REG_dst_idx         : std_logic_vector(4 downto 0);
-    pc, ALU_res, REG_src_2  : std_logic_vector(31 downto 0);
+    REG_we, MEM_we         : std_logic;
+    WB_src_ctrl            : std_logic_vector(1 downto 0);
+    MEM_op                 : std_logic_vector(3 downto 0);
+    REG_dst_idx            : std_logic_vector(4 downto 0);
+    pc, ALU_res, REG_src_2 : std_logic_vector(31 downto 0);
   end record t_exmem_reg;
 
   type t_memwb_reg is record
-  REG_we, MEM_we      : std_logic;
-  REG_dst_idx         : std_logic_vector(4 downto 0);
-  pc, ALU_res, MEM_out  : std_logic_vector(31 downto 0);
+    REG_we, MEM_we       : std_logic;
+    WB_src_ctrl          : std_logic_vector(1 downto 0);
+    REG_dst_idx          : std_logic_vector(4 downto 0);
+    pc, ALU_res, MEM_out : std_logic_vector(31 downto 0);
   end record t_memwb_reg;
 
-  end package records_pkg;
-
+end package records_pkg;
