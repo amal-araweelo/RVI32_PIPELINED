39eacfa240413a7851e4f55b2b48031be24079ed
Evangelos Lamprou <e.lamprou@upnet.gr>
2024-01-10 21:39:33 +0100

    add asm2vhdl helper tool

---
+++ tools/asm2vhdl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+N_LINE=0
+
+ASM_FILE=$1
+
+riscv32-unknown-elf-as -o output.o $ASM_FILE
+
+# Replace 'output.o' with your object file name
+riscv32-unknown-elf-objdump -d output.o | grep -E '^[[:space:]]+[0-9a-f]+:' | while read -r line; do
+    # Extract the hex value and the assembly code
+    hex_value=$(echo "$line" | awk '{print $2}')
+    asm_code=$(echo "$line" | cut -d$'\t' -f3-)
+
+    # Now you can use $hex_value and $asm_code as separate variables
+    echo "$N_LINE => x\"$hex_value\", -- $asm_code"
+
+    N_LINE=$((N_LINE+1))
+done
+
+rm output.o
