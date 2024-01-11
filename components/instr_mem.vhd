5003bbfee107dc2a980624696d7d7bb056e82bb8
Evangelos Lamprou <e.lamprou@upnet.gr>
2024-01-11 11:53:53 +0100

    smaller program in instr mem

--- components/instr_mem.vhd
+++ components/instr_mem.vhd
@@ -10,10 +10,10 @@ entity instr_mem is
   (
     -- Inputs
     clk      : in std_logic;
-    MEM_addr : in std_logic_vector(31 downto 0) := (others => '0'); -- address (it is the value stored in register 2)
+    MEM_addr : in std_logic_vector(31 downto 0) := (others => '0');
 
     -- Outputs
-    MEM_instr_out : out std_logic_vector(31 downto 0) := (others => '0')
+    MEM_instr_out : out std_logic_vector(31 downto 0)
   );
 end instr_mem;
 
@@ -21,21 +21,10 @@ architecture impl of instr_mem is
   type rom_type is array(2 ** 10 downto 0) -- 1 KiB
   of std_logic_vector (31 downto 0);
   signal rom : rom_type := (
-    1  => x"00000113", -- addi x2, x0, 0
-    2  => x"00100093", -- addi x1, x0, 1
-    3  => x"00000013", -- addi x0, x0, 0
-    4  => x"00000013", -- addi x0, x0, 0
-    5  => x"00108093", -- addi x1, x1, 1
-    6  => x"00000013", -- addi x0, x0, 0
-    7 => x"00000013", -- nop
-    8 => x"00508463",
-    9 => x"fe0008e3",
-    10 => x"00100113", 
-    11 => x"00000013",
-    12 => x"00000013",
-    13 =>  x"00000013",
-    14 => x"fc0006e3",
-  	    others => (x"00000000") 
+	0 => x"00500093", -- li ra,1
+	1 => x"00200113", -- li sp,2
+	2 => x"00300193", -- li gp,3
+	others => (x"00000000") 
     );
 
 begin
