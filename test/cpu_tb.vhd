81b42c34c7f1c60f6334e5e424d521c9086bd163
Evangelos Lamprou <e.lamprou@upnet.gr>
2024-01-11 13:26:43 +0100

    add WB_src_ctrl to cpu tb

--- test/cpu_tb.vhd
+++ test/cpu_tb.vhd
@@ -303,7 +303,7 @@ port map
 write_back_inst : write_back
 port map
 (
-	WB_src_ctrl => "01", -- TODO: Fix memwb_out.WB_src_ctrl,
+	WB_src_ctrl => memwb_out.WB_src_ctrl,
 	pc => memwb_out.pc,
 	ALU_res => memwb_out.ALU_res,
 	MEM_out => memwb_out.MEM_out,
@@ -321,6 +321,76 @@ end process;
 stim_proc: process
 begin
 
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
+    wait for clk_period;
     wait for clk_period;
     report "--------- CLOCK CYCLE ---------";
 
