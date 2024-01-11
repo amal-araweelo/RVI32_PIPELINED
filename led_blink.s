ca4f001a5ee8006874a2c79965a29ef127a3027c
amal-araweelo <amal-araweelo@github.com>
2024-01-11 11:14:47 +0100

    led test using sw

---
+++ led_blink.s
@@ -0,0 +1,21 @@
+.text
+addi x5, x0, 5
+reset:
+addi x2, x0, 0
+addi x1,x0,1
+addi x4, x0,1
+nop
+nop
+loop:
+    addi x1, x1, 1
+    nop
+    nop
+    beq x1, x5, blink
+    beq x0, x0, loop
+
+blink:
+    sw x4,0(x0)
+    nop
+    nop
+    nop
+    beq x0, x0, reset 
