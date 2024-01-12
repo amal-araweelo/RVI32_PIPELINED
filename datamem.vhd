6bee315017d1baa5aa14f1fd94dc8cac285a5f49
amal-araweelo <amal-araweelo@github.com>
2024-01-12 19:00:01 +0100

    removed pc from datamem

--- datamem.vhd
+++ datamem.vhd
@@ -1,4 +1,5 @@
 -- Data memory (only supports words right now)
+
 library ieee;
 library work;
 use ieee.std_logic_1164.all;
@@ -11,13 +12,12 @@ entity data_mem is
     -- Inputs
     clk         : in std_logic                     := '0'; -- clock
     MEM_we      : in std_logic                     := '0'; -- write enable
-    MEM_op      : in std_logic_vector(2 downto 0)  := (others => '0'); -- memory operation
+    MEM_op      : in std_logic_vector(3 downto 0)  := (others => '0'); -- memory operation
     MEM_data_in : in std_logic_vector(31 downto 0) := (others => '0');
     MEM_addr    : in std_logic_vector(31 downto 0) := (others => '0'); -- address (it is the value stored in register 2)
 
     -- Outputs
     MEM_data_out : out std_logic_vector(31 downto 0) := (others => '0')
-    -- blinky : out std_logic
   );
 end data_mem;
 
@@ -26,17 +26,14 @@ architecture impl of data_mem is
   of std_logic_vector (31 downto 0);
   signal ram : ram_type;
 
-
 begin
   process (clk) begin
-       -- Changes
     if (rising_edge(clk)) then
       MEM_data_out <= ram(to_integer(unsigned(MEM_addr))); -- data is always loaded from memory given addr
       case MEM_op is -- only implemented store word for now
         when sw =>
           if (MEM_we = '1') then
             ram(to_integer(unsigned(MEM_addr))) <= MEM_data_in;
-	    -- report "[DATA MEM] Storing " & to_string(MEM_data_in) & " at address " & to_string(MEM_addr); -- TODO: Delete
           end if;
         when others =>
           null;
