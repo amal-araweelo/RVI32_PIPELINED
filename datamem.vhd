3e56739e050395f86b9f37f65dcfe0d1f2d6cda2
amal-araweelo <amal-araweelo@github.com>
2024-01-16 05:22:34 +0100

    new datamemory is tested and passes random tests

--- datamem.vhd
+++ datamem.vhd
@@ -24,30 +24,35 @@ entity data_mem is
 end data_mem;
 
 architecture impl of data_mem is
-  type quad_port_ram_type is array(2 ** 10 downto 0) -- 1 KiB
+  type ram_type is array(2 ** 10 downto 0) -- 1 KiB
   of std_logic_vector (7 downto 0);
-  signal quad_port_ram : ram_type;
-  signal read_data     : std_logic_vector(31 downto 0);
-  signal write_data    : std_logic_vector(31 downto 0);
-  signal MEM_IO        : std_logic_vector(31 downto 0);
-  --alias MEM_sel     : std_logic is MEM_addr(31);
+  signal ram        : ram_type := (others => (others => '0'));
+  signal read_data  : std_logic_vector(31 downto 0);
+  signal write_data : std_logic_vector(31 downto 0);
+  signal MEM_IO     : std_logic_vector(31 downto 0);
 
   -- To be loaded from or stored at different bytes
-  signal load_from_0 : std_logic_vector(7 downto 0);
+  signal load_from_0 : std_logic_vector(7 downto 0); -- LSB
   signal load_from_1 : std_logic_vector(7 downto 0);
   signal load_from_2 : std_logic_vector(7 downto 0);
-  signal load_from_3 : std_logic_vector(7 downto 0);
+  signal load_from_3 : std_logic_vector(7 downto 0); -- MSB
 
-  signal store_at_0 : std_logic_vector(7 downto 0);
+  signal store_at_0 : std_logic_vector(7 downto 0); -- LSB
   signal store_at_1 : std_logic_vector(7 downto 0);
   signal store_at_2 : std_logic_vector(7 downto 0);
-  signal store_at_3 : std_logic_vector(7 downto 0);
+  signal store_at_3 : std_logic_vector(7 downto 0); -- MSB
 
-  -- Write enable for the 4 bytes
+  -- Write enable for the 4 bytes to be stored
   signal we_0 : std_logic;
   signal we_1 : std_logic;
   signal we_2 : std_logic;
   signal we_3 : std_logic;
+
+  -- Memory address for the 4 bytes
+  signal MEM_addr_0 : std_logic_vector(31 downto 0);
+  signal MEM_addr_1 : std_logic_vector(31 downto 0);
+  signal MEM_addr_2 : std_logic_vector(31 downto 0);
+  signal MEM_addr_3 : std_logic_vector(31 downto 0);
 
 begin
   -- Synchronous write
@@ -56,104 +61,143 @@ begin
       if (MEM_we = '1') then
         if (MEM_addr = x"00000000") then
           MEM_IO <= write_data;
-          elsif (we_0 = '1') then
-          quad_port_ram(to_integer(unsigned(MEM_addr))) <= store_at_0;
-          elsif (we_1 = '1') then
-          quad_port_ram(to_integer(unsigned(MEM_addr))) <= store_at_1;
-          elsif (we_2 = '1') then
-          quad_port_ram(to_integer(unsigned(MEM_addr))) <= store_at_2;
-          elsif (we_3 = '1') then
-          quad_port_ram(to_integer(unsigned(MEM_addr))) <= store_at_3;
-          --ram(to_integer(unsigned(MEM_addr))) <= write_data;
+        end if;
+        if (we_0 = '1') then
+          ram(to_integer(unsigned(MEM_addr_0))) <= store_at_0;
+        end if;
+        if (we_1 = '1') then
+          ram(to_integer(unsigned(MEM_addr_1))) <= store_at_1;
+        end if;
+        if (we_2 = '1') then
+          ram(to_integer(unsigned(MEM_addr_2))) <= store_at_2;
+        end if;
+        if (we_3 = '1') then
+          ram(to_integer(unsigned(MEM_addr_3))) <= store_at_3;
         end if;
       end if;
     end if;
   end process;
 
   -- Asynchronous read
-  process (all) begin
-    -- Intialization
-    load_from_0 <= x"0000";
-    load_from_1 <= x"0000";
-    load_from_2 <= x"0000";
-    load_from_3 <= x"0000";
+  process (all)
+  begin
+    -- Intialization of bytes to be loaded from or stored in memory
+    load_from_0 <= x"00";
+    load_from_1 <= x"00";
+    load_from_2 <= x"00";
+    load_from_3 <= x"00";
 
-    store_at_0 <= x"0000";
-    store_at_1 <= x"0000";
-    store_at_2 <= x"0000";
-    store_at_3 <= x"0000";
+    store_at_0 <= x"00";
+    store_at_1 <= x"00";
+    store_at_2 <= x"00";
+    store_at_3 <= x"00";
 
+    -- Initialization of write-enables
     we_0 <= '0';
     we_1 <= '0';
     we_2 <= '0';
     we_3 <= '0';
 
-    -- Initialize write_data to a default value
-    write_data <= (others => '0');
-    read_data  <= (others => '0');
+    -- Initialization of the memory adress of the 4 bytes
+    MEM_addr_0 <= MEM_addr;
+    MEM_addr_1 <= std_logic_vector(unsigned(MEM_addr) + to_unsigned(1, 31)); -- offset with 1 byte
+    MEM_addr_2 <= std_logic_vector(unsigned(MEM_addr) + to_unsigned(2, 31)); -- offset with 2 bytes
+    MEM_addr_3 <= std_logic_vector(unsigned(MEM_addr) + to_unsigned(3, 31)); -- offset with 3 bytes
+
+    report "MEM_ADDR0: " & to_String(MEM_addr_0);
+    report "MEM_ADDR1: " & to_String(MEM_addr_1);
+    report "MEM_ADDR2: " & to_String(MEM_addr_2);
+    report "MEM_ADDR3: " & to_String(MEM_addr_3);
+
     case MEM_op is
-      when lw =>
-        read_data <= ram(to_integer(unsigned(MEM_addr)));
-      when lh =>
-        read_data(15 downto 0) <= ram(to_integer(unsigned(MEM_addr)))(15 downto 0);
-        if read_data(15) = '1' then
-          -- Sign extension for negative value
-          read_data(31 downto 16) <= (others => '1');
+      when lw => -- Load word
+
+        load_from_0 <= ram(to_integer(unsigned(MEM_addr_0)));
+        load_from_1 <= ram(to_integer(unsigned(MEM_addr_1)));
+        load_from_2 <= ram(to_integer(unsigned(MEM_addr_2)));
+        load_from_3 <= ram(to_integer(unsigned(MEM_addr_3)));
+
+        report "load0: " & to_string(load_from_0);
+        report "load1: " & to_string(load_from_1);
+        report "load2: " & to_string(load_from_2);
+        report "load3: " & to_string(load_from_3);
+
+      when lh => -- Load half word (signed)
+
+        load_from_0 <= ram(to_integer(unsigned(MEM_addr_0)));
+        load_from_1 <= ram(to_integer(unsigned(MEM_addr_1)));
+
+        if load_from_1(7) = '1' then -- Sign-extension for negative value
+          load_from_2 <= (others => '1');
+          load_from_3 <= (others => '1');
         else
-          read_data(31 downto 16) <= (others => '0');
+          load_from_2 <= (others => '0');-- Sign-extension for positive value
+          load_from_3 <= (others => '0');
         end if;
-      when lhu =>
-        read_data(15 downto 0)  <= ram(to_integer(unsigned(MEM_addr)))(15 downto 0);
-        read_data(31 downto 16) <= (others => '0'); -- Zero-extend
-      when lb                            =>
-        read_data(7 downto 0) <= ram(to_integer(unsigned(MEM_addr)))(7 downto 0);
-        if read_data(7) = '1' then
-          -- Sign extension for negative value
-          read_data(31 downto 8) <= (others => '1');
+
+      when lhu => -- Load half word (unsigned)
+
+        load_from_0 <= ram(to_integer(unsigned(MEM_addr_0)));
+        load_from_1 <= ram(to_integer(unsigned(MEM_addr_1)));
+        load_from_2 <= (others => '0');
+        load_from_3 <= (others => '0');
+
+      when lb => -- Load byte (signed)
+        load_from_0 <= ram(to_integer(unsigned(MEM_addr_0)));
+
+        if load_from_0(7) = '1' then
+          load_from_1 <= (others => '1'); -- Sign extension for negative value
+          load_from_2 <= (others => '1');
+          load_from_3 <= (others => '1');
         else
-          read_data(31 downto 8) <= (others => '0');
+          load_from_1 <= (others => '0'); -- Sign extension for positive value
+          load_from_2 <= (others => '0');
+          load_from_3 <= (others => '0');
         end if;
-      when lbu =>
-        read_data(7 downto 0)  <= ram(to_integer(unsigned(MEM_addr)))(7 downto 0);
-        read_data(31 downto 8) <= (others => '0'); -- Zero-extend
-      when sw                           =>
-        we_b0 <= '1'; -- enable write to all bytes
-        we_b1 <= '1';
-        we_b2 <= '1';
-        we_b3 <= '1';
+
+      when lbu => -- Load byte (unsigned)
+        load_from_0 <= ram(to_integer(unsigned(MEM_addr_0)));
+        load_from_1 <= (others => '0');
+        load_from_2 <= (others => '0');
+        load_from_3 <= (others => '0');
+
+      when sw => -- Store word
+        we_0 <= '1'; -- enable write to all bytes
+        we_1 <= '1';
+        we_2 <= '1';
+        we_3 <= '1';
 
         store_at_3 <= MEM_data_in(31 downto 24);
-        store_at_2 <= MEM_data_in(25 downto 16);
+        store_at_2 <= MEM_data_in(23 downto 16);
         store_at_1 <= MEM_data_in(15 downto 8);
         store_at_0 <= MEM_data_in(7 downto 0);
-        write_data <= MEM_data_in(31 downto 0);
-      when sh =>
-        we_b0 <= '1'; -- only enable write to byte 0 and byte 1
-        we_b1 <= '1';
-        we_b2 <= '0';
-        we_b3 <= '0';
+      when sh => -- Store half word
+        we_0 <= '1'; -- only enable write to byte 0 and byte 1
+        we_1 <= '1';
+        we_2 <= '0';
+        we_3 <= '0';
 
         store_at_1 <= MEM_data_in(15 downto 8);
         store_at_0 <= MEM_data_in(7 downto 0);
 
-        write_data(15 downto 0)   <= MEM_data_in(15 downto 0);
-        write_data (31 downto 16) <= (others => '0'); -- fill up with 0's
-      when sb                              =>
-        we_b0 <= '1'; -- only enable write to byte 0
-        we_b1 <= '0';
-        we_b2 <= '0';
-        we_b3 <= '0';
+      when sb => -- Store byte
+        we_0 <= '1'; -- only enable write to byte 0
+        we_1 <= '0';
+        we_2 <= '0';
+        we_3 <= '0';
 
         store_at_0 <= MEM_data_in(7 downto 0);
 
-        write_data(7 downto 0)    <= MEM_data_in(7 downto 0);
-        write_data (31 downto 16) <= (others => '0'); -- fill up with 0's
-      when others                          =>
+      when others =>
         null;
-        --write_data <= x"00000000";
-        --read_data  <= x"00000000";
     end case;
-    MEM_data_out <= read_data;
+    MEM_data_out <= load_from_3 & load_from_2 & load_from_1 & load_from_0; -- concatenate the four bytes read from memory
     MEM_IO_out   <= MEM_IO;
+    -- debugging
+    report "MEM_data_out: " & to_string(MEM_data_out);
+    report "LOAD 0: " & to_string(load_from_0);
+    report "LOAD 1: " & to_string(load_from_1);
+    report "LOAD 2: " & to_string(load_from_2);
+    report "LOAD 3: " & to_string(load_from_3);
   end process;
 end architecture;
--- test/datamem_tb.vhd
+++ test/datamem_tb.vhd
@@ -1,58 +1,49 @@
 library ieee;
 library work;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+use std.env.stop;
 use work.mem_op_const.all;
 
-entity datamem_tb is
-  -- Testbench has no ports
-end datamem_tb;
+entity data_mem_tb is
+end data_mem_tb;
 
-architecture behavorial of datamem_tb is
+architecture tb_arch of data_mem_tb is
+  signal clk          : std_logic                     := '0';
+  signal MEM_we       : std_logic                     := '0';
+  signal MEM_op       : std_logic_vector(2 downto 0)  := (others => '0');
+  signal MEM_data_in  : std_logic_vector(31 downto 0) := (others => '0');
+  signal MEM_addr     : std_logic_vector(31 downto 0) := (others => '0');
+  signal MEM_data_out : std_logic_vector(31 downto 0);
+  signal MEM_IO_out   : std_logic_vector(31 downto 0);
 
-  -- Component Declaration for the Unit Under Test (UUT)
-  component data_mem is
+  constant CLK_PERIOD : time := 10 ns;
+
+  component data_mem
     port
     (
-      -- Inputs
-      clk         : in std_logic                     := '0'; -- clock
-      MEM_we      : in std_logic                     := '0'; -- write enable
-      MEM_op      : in std_logic_vector              := "000"; -- memory operation
-      MEM_data_in : in std_logic_vector(31 downto 0) := (others => '0');
-      MEM_addr    : in std_logic_vector(31 downto 0) := (others => '0'); -- address (it is the value stored in register 2)
-
-      -- Outputs
-      MEM_data_out : out std_logic_vector(31 downto 0) := (others => '0')
+      clk          : in std_logic;
+      MEM_we       : in std_logic;
+      MEM_op       : in std_logic_vector(2 downto 0);
+      MEM_data_in  : in std_logic_vector(31 downto 0);
+      MEM_addr     : in std_logic_vector(31 downto 0);
+      MEM_data_out : out std_logic_vector(31 downto 0);
+      MEM_IO_out   : out std_logic_vector(31 downto 0)
     );
   end component;
 
-  -- Inputs
-  signal clk         : std_logic                     := '0'; -- clock
-  signal MEM_we      : std_logic                     := '0'; -- write enable
-  signal MEM_op      : std_logic_vector(2 downto 0)  := (others => '0'); -- memory operation
-  signal MEM_data_in : std_logic_vector(31 downto 0) := (others => '0');
-  signal MEM_addr    : std_logic_vector(31 downto 0) := (others => '0'); -- address (it is the value stored in register 2)
-
-  -- Outputs
-  signal MEM_data_out : std_logic_vector(31 downto 0) := (others => '0');
-
-  -- clk period definitions
-  constant clk_period : time := 100 ns;
-
 begin
-
-  -- Instantiate the Unit Under Test (UUT)
-  uut : data_mem port map
+  UUT : data_mem port map
   (
     clk          => clk,
     MEM_we       => MEM_we,
     MEM_op       => MEM_op,
     MEM_data_in  => MEM_data_in,
     MEM_addr     => MEM_addr,
-    MEM_data_out => MEM_data_out
+    MEM_data_out => MEM_data_out,
+    MEM_IO_out   => MEM_IO_out
   );
 
-  -- Clock process
+  -- clk_in process definitions
   clk_process : process
   begin
     clk <= '0';
@@ -61,73 +52,82 @@ begin
     wait for clk_period/2;
   end process;
 
-  --   Stimulus process
-  stimulus : process
+  -- Stimulus process
+  process
   begin
+    wait for 5 * CLK_PERIOD;
 
-    -- Test for load and store word
-    MEM_op      <= sw; -- Store word operation
+    -- Test 1: Store word in memory and load same word from memory
     MEM_we      <= '1';
-    MEM_data_in <= x"12345678"; -- Example data for store word
+    MEM_data_in <= x"12345678";
+    MEM_op      <= sw;
     MEM_addr    <= x"00000001";
-    wait for clk_period;
 
-    MEM_we   <= '0';
-    MEM_op   <= lw; -- Load word operation
-    MEM_addr <= x"00000001";
-    wait for clk_period;
+    wait for 5 * CLK_PERIOD;
 
-    assert MEM_data_out = x"12345678" report "Test for store word and load word failed" severity error;
-    report "Test for store word and load word [PASSED]";
+    MEM_we <= '0';
+    MEM_op <= lw;
 
-    -- Test for load and store half-word
-    MEM_op      <= sh; -- Store half-word operation
+    wait for CLK_PERIOD;
+
+    if MEM_data_out = x"12345678" then
+      report "Test 1 [PASSED]";
+      else
+      report "Test 1 [FAILED]:  MEM_data_out = " & to_string(MEM_data_out);
+      std.env.stop(1);
+    end if;
+
+    -- Test 2: Load half word from memory (unsigned)
+    MEM_we <= '0';
+    MEM_op <= lhu;
+
+    wait for CLK_PERIOD;
+    if MEM_data_out = x"00005678" then
+      report "Test 2 [PASSED]";
+      else
+      report "Test 2 [FAILED]: MEM_data_out =" & to_string(MEM_data_out);
+      std.env.stop(1);
+    end if;
+
+    -- Test 3: Store half word and load half word from memory (signed)
     MEM_we      <= '1';
-    MEM_data_in <= x"0000ABCD"; -- Example data for store half-word
-    MEM_addr    <= x"00000004"; -- Address for storing half-word
-    wait for clk_period;
+    MEM_data_in <= x"0000D6F8";
+    MEM_op      <= sh;
+    MEM_addr    <= x"00000002";
 
-    MEM_we   <= '0';
-    MEM_op   <= lh; -- Load half-word operation
-    MEM_addr <= x"00000004"; -- Address for loading half-word
-    wait for clk_period;
+    wait for CLK_PERIOD;
+    MEM_we <= '0';
+    MEM_op <= lh;
 
-    assert MEM_data_out = x"FFFFABCD" report "Test for store half-word and load half-word failed" severity error;
-    report "Test for store half-word and load half-word [PASSED]";
+    wait for CLK_PERIOD;
+    if MEM_data_out = x"FFFFD6F8" then
+      report "Test 3 [PASSED]";
+      else
+      report "Test 3 [FAILED]: MEM_data_out =" & to_string(MEM_data_out);
+      std.env.stop(1);
+    end if;
 
-    MEM_op <= lhu; -- Load half-word unsigned operation
-    wait for clk_period;
-    assert MEM_data_out = x"0000ABCD" report "Test for load half-word unsigned failed" severity error;
-    report "Test for store half-word and load half-word [PASSED]";
+    -- Test 4: Load byte from memory (signed)
+    MEM_we <= '0';
+    MEM_op <= lb;
 
-    -- Test for store byte and load byte
-    MEM_op      <= sb; -- Store byte operation
-    MEM_we      <= '1';
-    MEM_data_in <= x"000000EF"; -- Example data for store byte
-    MEM_addr    <= x"00000008"; -- Address for storing byte
-    wait for clk_period;
+    wait for CLK_PERIOD;
+    if MEM_data_out = x"FFFFFFF8" then
+      report "Test 4 [PASSED]";
+      else
+      report "Test 4 [FAILED]: MEM_data_out =" & to_string(MEM_data_out);
+      std.env.stop(1);
+    end if;
 
-    -- Test for store byte and load byte
-    MEM_op      <= sb; -- Store byte operation
-    MEM_we      <= '1';
-    MEM_data_in <= x"000000EF"; -- Example data for store byte
-    MEM_addr    <= x"00000008"; -- Address for storing byte
-    wait for clk_period;
-
-    MEM_we   <= '0';
-    MEM_op   <= lb; -- Load byte operation
-    MEM_addr <= x"00000008"; -- Address for loading byte
-    wait for clk_period;
-    assert MEM_data_out = x"FFFFFFEF" report "Test for store byte and load byte failed" severity error;
-    report "Test for store byte and load byte [PASSED]";
-
-    MEM_op   <= lbu; -- Load byte unsigned operation
-    MEM_addr <= x"00000008"; -- Address for loading byte
-    wait for clk_period;
-    assert MEM_data_out = x"000000EF" report "Test for load unsigned byte failed" severity error;
-    report "Test for store byte and load byte [PASSED]";
-
-    std.env.stop(0);
+    -- Test 5: Load byte unsigned from memory (unsigned)
+    MEM_op <= lbu;
+    wait for CLK_PERIOD;
+    if MEM_data_out = x"000000F8" then
+      report "Test 5 [PASSED]";
+      else
+      report "Test 5 [FAILED]: MEM_data_out =" & to_string(MEM_data_out);
+      std.env.stop(1);
+    end if;
+    wait;
   end process;
-
-end behavorial;
+end tb_arch;
