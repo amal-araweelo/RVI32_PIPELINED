afc09c452390a87d9c9a61b914a49d822d10aec1
amal-araweelo <amal-araweelo@github.com>
2024-01-17 11:36:25 +0100

    handling case where we read from and write to same register

--- registerfile.vhd
+++ registerfile.vhd
@@ -1,11 +1,11 @@
 library ieee;
+library work;
 use ieee.std_logic_1164.all;
 use work.records_pkg.all;
 use work.alu_ctrl_const.all;
 use work.const_decoder.all;
+use work.reg_file_pkg.all;
 use work.mem_op_const.all;
-
-library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
@@ -30,7 +30,9 @@ end reg_file;
 architecture behavioral of reg_file is
   type registerfile_type is array (2 ** W - 1 downto 0) of
   std_logic_vector(B - 1 downto 0);
-  signal array_register : registerfile_type := (others => (others => '0'));
+  signal array_register         : registerfile_type := (others => (others => '0'));
+  signal do_forward_reg_current : std_logic;
+  signal do_forward_reg_next    : std_logic;
 
 begin
   -- Clock process
@@ -41,21 +43,30 @@ begin
         --report "[REG FILE] Reg(" & to_string(REG_dst_idx) & ") <= " & to_string(REG_write_data);
         array_register(to_integer(unsigned(REG_dst_idx))) <= REG_write_data;
       end if;
+
     end if;
   end process;
 
   process (all)
   begin
-    -- Asynchronous read
-    REG_src_1 <= array_register(to_integer(unsigned(REG_src_idx_1)));
-    REG_src_2 <= array_register(to_integer(unsigned(REG_src_idx_2)));
+    -- Handling case where we write and read to same register
+    if (REG_we = '1' and (REG_src_idx_1 = REG_dst_idx)) then
+      REG_src_1 <= REG_write_data;
+    end if;
+    if (REG_we = '1' and (REG_src_idx_2 = REG_dst_idx)) then
+      REG_src_2 <= REG_write_data;
+    end if;
 
     -- Handling x0
     if (REG_src_idx_1 = "00000") then
       REG_src_1 <= x"00000000";
     end if;
+
     if (REG_src_idx_2 = "00000") then
       REG_src_2 <= x"00000000";
     end if;
+    -- Asynchronous read
+    REG_src_1 <= array_register(to_integer(unsigned(REG_src_idx_1)));
+    REG_src_2 <= array_register(to_integer(unsigned(REG_src_idx_2)));
   end process;
 end behavioral;
