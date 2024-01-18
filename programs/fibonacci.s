65066c48c266b4b148b2bc2e8c9cbb2fc1413e9c
amal-araweelo <amal-araweelo@github.com>
2024-01-18 11:28:53 +0100

    fixed commas in fibonacci programs

--- programs/fibonacci.s
+++ programs/fibonacci.s
@@ -1,33 +1,34 @@
-addi x1 x0 1
-addi x2 x0 0
-li x3, 0x1da31 #highest fibbonaci number below limit
-li x4 0x10000018 #address of fibbonaci storage
-addi x5 x0 1
+# fibonnaci
+addi x1, x0, 1
+addi x2, x0, 0
+li x3, 0xB520 #highest fibbonaci number below limit
+li x4, 0x10000018 #address of fibbonaci storage
+addi x5, x0, 1
 
 restart:
-sw x0 0(x4)
-addi x6 x0 0
+sw x0, 0(x4)
+addi x6, x0, 0
 loopup:
-    add x2 x1 x2
-    add x6 x2 x0
-    sw x2 0(x4)
+    add x2, x1, x2
+    add x6, x2, x0
+    sw x2, 0(x4)
     
-    beq x2 x3 loopdown
+    beq x2, x3, loopdown
     
-    add x1 x1 x2
-    add x6 x1 x0
-    sw x1 0(x4)
+    add x1, x1, x2
+    add x6, x1, x0
+    sw x1, 0(x4)
     
-    beq x0 x0 loopup
+    beq x0, x0, loopup
 loopdown:
     
-    sub x2 x2 x1
-    beq x1 x5 restart
-    add x6 x1 x0
-    sw x1 0(x4)
+    sub x2, x2, x1
+    beq x1, x5, restart
+    add x6, x1, x0
+    sw x1, 0(x4)
     
-    sub x1 x1 x2
-    add x6 x2 x0
-    sw x2 0(x4)
+    sub x1, x1, x2
+    add x6, x2, x0
+    sw x2, 0(x4)
     
-    beq x0 x0 loopdown
+    beq x0, x0, loopdown
