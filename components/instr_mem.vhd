1cc010f858731d6bd5a3a7148dad615e34644e09
amal-araweelo <amal-araweelo@github.com>
2024-01-18 08:17:24 +0100

    led blink pattern program

--- components/instr_mem.vhd
+++ components/instr_mem.vhd
@@ -18,15 +18,30 @@ entity instr_mem is
 end instr_mem;
 
 architecture impl of instr_mem is
-  type rom_type is array(2 ** 10 downto 0) -- 1 KiB
+  type rom_type is array(2 ** 7 downto 0) -- 1 KiB
   of std_logic_vector (31 downto 0);
 
   signal rom : rom_type := (
-
-0 => x"00000093", -- li	ra,0
-1 => x"00108093", -- addi	ra,ra,1
-2 => x"00108093", -- addi	ra,ra,1
-  
+  0      => x"00000093", -- li ra,0
+  1      => x"00100113", -- li sp,1
+  2      => x"00000193", -- li gp,0
+  3      => x"000081b7", -- lui        gp,0x8
+  4      => x"00018193", -- mv gp,gp
+  5      => x"00000213", -- li tp,0
+  6      => x"000002b7", -- lui        t0,0x0
+  7      => x"1f428293", -- addi       t0,t0,500 # 1f4 <waitloop+0x1b8>
+  8      => x"00100113", -- li sp,1
+  9      => x"0020a023", -- sw sp,0(ra)
+  10     => x"00000a63", -- beqz      zero,3c <waitloop>
+  11     => x"00000213", -- li        tp,0
+  12     => x"00111113", -- slli      sp,sp,0x1
+  13     => x"0020a023", -- sw        sp,0(ra)
+  14     => x"00000263", -- beqz      zero,3c <waitloop>
+  15     => x"00120213", -- addi      tp,tp,1 # 1 <reset-0x1f>
+  16     => x"fe521ee3", -- bne       tp,t0,3c <waitloop>
+  17     => x"00000213", -- li        tp,0
+  18     => x"fc310ce3", -- beq       sp,gp,20 <reset>
+  19     => x"fe0000e3", -- beqz      zero,2c <blinkloop>
   others => (x"00000000")
   );
   --attribute rom_style        : string;
--- programs/led_blink.s
+++ programs/led_blink.s
@@ -1,31 +1,28 @@
-.text
+    ### Led blinky time 2.0
+addi x1, x0, 0    #LED storage location
+addi x2, x0, 1
+addi x3, x0, 0
+lui x3, 8        #Max blink value (2^^15)
+addi x3, x3, 0    #Max blink value (2^^15)
+addi x4, x0, 0     #Counter for waitloop
+lui x5, 0       #989680 is 10'000'000 (should make waitloop half sec bc 2 instr)
+addi x5, x5, 500    #989680 is 10'000'000 (should make waitloop half sec bc 2 instr)
+
 reset:
-    addi x1, x0, 0
-    addi x2, x0, 1
-    addi x3, x0, 5
-    nop
-    nop
-    nop
-    sw x2,0(x0)
-    nop
-    nop
-    nop
+addi x2, x0, 1    #LED value
+sw x2, 0(x1)
+beq x0, x0, waitloop
 
-loop:
-    nop
-    nop
-    addi x1,x1,1
-    nop
-    nop
-    nop
-    beq x1, x3, blink
-    beq x0, x0, loop
+blinkloop:
+addi x4, x0, 0
+slli x2, x2, 1
+sw x2, 0(x1)
+beq x0, x0, waitloop
 
-blink:
-    lw x2, 0(x0)
-    nop
-    nop
-    nop
-    addi x2,x0,0
-    beq x0,x0,reset
-
+waitloop:        #Loop to slow down execution speed
+addi x4, x4, 1
+bne x4, x5, waitloop
+addi x4, x0, 0
+beq x2, x3, reset
+beq x0, x0, blinkloop
+ 
